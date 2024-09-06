c3dda2bbc1085d6d84a748b20e2206673cb41a32
Tiga Shabani <tigana.saabani@gmail.com>
2024-09-06 19:26:43 +0200

    create routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from "../services/userService.js";
+import { login } from "../services/authService.js";
+import { authenticateToken } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+  try {
+    const token = await login(req.body.username, req.body.password);
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(401).json({ error: "Invalid credentials" });
+  }
+});
+
+router.get("/users", getAllUsers);
+router.post("/users", authenticateToken, createUser);
+router.get("/users/:id", getUserById);
+router.put("/users/:id", authenticateToken, updateUser);
+router.delete("/users/:id", authenticateToken, deleteUser);
+
+export default router;
