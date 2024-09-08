bb62ee38e95e3501a79705709ee6134708ee1986
Tiga Shabani <tigana.saabani@gmail.com>
2024-09-08 12:41:22 +0200

    add routes

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 import express from "express";
-import dotenv from "dotenv";
 
 import userRoutes from "./routes/userRoutes.js";
-//import hostRoutes from './routes/hostRoutes.js';
-//import propertyRoutes from './routes/propertyRoutes.js';
-//import amenityRoutes from './routes/amenityRoutes.js';
-//import bookingRoutes from './routes/bookingRoutes.js';
-//import reviewRoutes from './routes/reviewRoutes.js';
+import hostRoutes from "./routes/hostRoutes.js";
+import propertyRoutes from "./routes/propertyRoutes.js";
+import amenityRoutes from "./routes/amenityRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", userRoutes);
-//app.use('/api', hostRoutes);
-//app.use('/api', propertyRoutes);
-//app.use('/api', amenityRoutes);
-//app.use('/api', bookingRoutes);
-//app.use('/api', reviewRoutes);
+app.use("/api", hostRoutes);
+app.use("/api", propertyRoutes);
+app.use("/api", amenityRoutes);
+app.use("/api", bookingRoutes);
+app.use("/api", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
