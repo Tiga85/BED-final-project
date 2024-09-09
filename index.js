ad942a36faaf5494ed374a1f968ae78f15d52b11
Tiga Shabani <tigana.saabani@gmail.com>
2024-09-09 13:54:04 +0200

    update for use with sentry

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//import instrument from "./sentry/instrument.js";
+
+//import  Sentry from "@sentry/node"; 
+
 import express from "express";
 
 import userRoutes from "./routes/userRoutes.js";
@@ -11,6 +15,24 @@ const app = express();
 
 app.use(express.json());
 
+/*
+app.get("/", function rootHandler (req, res) {
+  res.send("Hello world!");
+});
+
+// The error handler must be registered before any other error middleware and after all controllers
+Sentry.setupExpressErrorHandler(app);
+
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
+
+*/
 app.use("/api", userRoutes);
 app.use("/api", hostRoutes);
 app.use("/api", propertyRoutes);
@@ -18,10 +40,11 @@ app.use("/api", amenityRoutes);
 app.use("/api", bookingRoutes);
 app.use("/api", reviewRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
-
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
+});
+
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
 });
