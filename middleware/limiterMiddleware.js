7a28b69893c440eba8dea07e062686bedeb8c127
Tiga Shabani <tigana.saabani@gmail.com>
2024-09-11 10:47:57 +0200

    expermintal limiter middleware

---
+++ middleware/limiterMiddleware.js
@@ -0,0 +1,11 @@
+/* 
+// Create a rate limiter middleware
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 5, // Allow 5 requests per minute
+    message: 'Too many requests from this IP, please try again later.',
+ });
+  // Apply the rate limiter middleware to all requests
+ app.use(limiter);
+
+ */
