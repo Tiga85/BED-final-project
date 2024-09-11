a0b1d1d99b1a6972438058afe591657d382d1827
Tiga Shabani <tigana.saabani@gmail.com>
2024-09-11 10:47:31 +0200

    add dataRoutes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //import instrument from "./sentry/instrument.js";
 
-//import  Sentry from "@sentry/node"; 
+//import  Sentry from "@sentry/node";
 
 import express from "express";
 
@@ -10,41 +10,26 @@ import propertyRoutes from "./routes/propertyRoutes.js";
 import amenityRoutes from "./routes/amenityRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
+import dataRoutes from "./routes/dataRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 
-/*
-app.get("/", function rootHandler (req, res) {
-  res.send("Hello world!");
-});
+//app.use("/docs", express.static('docs'));
 
-// The error handler must be registered before any other error middleware and after all controllers
-Sentry.setupExpressErrorHandler(app);
-
-
-// Optional fallthrough error handler
-app.use(function onError(err, req, res, next) {
-  // The error id is attached to `res.sentry` to be returned
-  // and optionally displayed to the user for support.
-  res.statusCode = 500;
-  res.end(res.sentry + "\n");
-});
-
-*/
 app.use("/api", userRoutes);
 app.use("/api", hostRoutes);
 app.use("/api", propertyRoutes);
 app.use("/api", amenityRoutes);
 app.use("/api", bookingRoutes);
 app.use("/api", reviewRoutes);
+app.use("/api", dataRoutes);
+
+app.get("/", (req, res) => {
+  res.send("BED final project: Booking API ");
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
-});
-
-
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
 });
