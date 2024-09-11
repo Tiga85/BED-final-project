d74c7b06062a4f4852cef3301340577c876567a5
Tiga Shabani <tigana.saabani@gmail.com>
2024-09-11 20:08:40 +0200

    add auth0 advanced authontication

---
+++ middleware/advancedAuth.js
@@ -0,0 +1,19 @@
+import { auth } from "express-oauth2-jwt-bearer";
+import request from "request";
+
+export const authMiddleware = auth({
+  audience: "https//bookings-api", // e.g. https://bookkings-api
+  issuerBaseURL: `https://dev-xtgdrd6laklrwy8u.us.auth0.com/`,
+});
+
+// print token for testing 
+const options = { method: 'POST',
+    url: 'https://dev-xtgdrd6laklrwy8u.us.auth0.com/oauth/token',
+    headers: { 'content-type': 'application/json' },
+    body: '{"client_id":"V9kXNe65441BntFf8PHfDOqm5C88PBeE","client_secret":"M1JEbWJIRmZCxCd7AvNp74BdqXoZ-ZJKbO_wOnTJO9GBqz4rudFU_UrT-RE1M6Zt","audience":"https//bookings-api","grant_type":"client_credentials"}' };
+  
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  
+    console.log(body);
+  });
