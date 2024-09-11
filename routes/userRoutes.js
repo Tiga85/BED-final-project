b32101724ecfc9fb5860c3f6eba1700312524816
Tiga Shabani <tigana.saabani@gmail.com>
2024-09-11 17:21:41 +0200

    remove login from userroutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,19 +6,10 @@ import {
   updateUser,
   deleteUser,
 } from "../services/userService.js";
-import { login } from "../services/authService.js";
+
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-
-router.post("/login", async (req, res) => {
-  try {
-    const token = await login(req.body.username, req.body.password);
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(401).json({ error: "Invalid credentials" });
-  }
-});
 
 router.get("/users",  getAllUsers);
 router.post("/users", authenticateToken, createUser);
