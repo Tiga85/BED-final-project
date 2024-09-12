053997595875b9f84cf4c26dd57f34c8f09c9bc8
Tiga Shabani <tigana.saabani@gmail.com>
2024-09-12 17:32:03 +0200

    use sentry and error handlers

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-//import instrument from "./sentry/instrument.js";
-
-//import  Sentry from "@sentry/node";
-
+import * as Sentry from "@sentry/node";
 import express from "express";
-import 'dotenv/config';
-import { logMiddleware } from "./middleware/logMiddleware.js";
+import "dotenv/config";
+
+import logMiddleware from "./middleware/logMiddleware.js";
 import loginRouter from "./routes/loginRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import hostRoutes from "./routes/hostRoutes.js";
@@ -13,13 +11,28 @@ import amenityRoutes from "./routes/amenityRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
 import dataRoutes from "./routes/dataRoutes.js";
+import notFoundErrorHandler from "./middleware/notFoundErrorHandler.js";
+import errorHandler from "./errors/errorHandler.js";
+
+// Custom integration example
+class CustomIntegration {
+  setupOnce() {
+    console.log("Custom integration setup");
+  }
+}
+
+Sentry.init({
+  dsn: "https://0d82d19c2231f0a637d03a0044c55b9a@o4507921963679744.ingest.de.sentry.io/4507921985896528",
+  integrations: [new CustomIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
 
 const app = express();
 app.use(express.json());
-
 app.use(logMiddleware);
-
-//app.use("/docs", express.static('docs'));
 
 app.use("/api", loginRouter);
 app.use("/api", userRoutes);
@@ -31,8 +44,15 @@ app.use("/api", reviewRoutes);
 app.use("/api", dataRoutes);
 
 app.get("/", (req, res) => {
-  res.send("BED final project: Booking API ");
+  const html = "<h1>BED final project: Booking API </h1>";
+  res.send(html);
 });
+
+// Use Sentry's error handler middleware
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(notFoundErrorHandler);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
