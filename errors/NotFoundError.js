a2bf670ce5e52323fc66e4b35624e10a38ce30d3
Tiga Shabani <tigana.saabani@gmail.com>
2024-09-13 13:29:03 +0200

    Updated

--- errors/NotFoundError.js
+++ errors/NotFoundError.js
@@ -1,11 +1,8 @@
-
-
 class NotFoundError extends Error {
-    constructor(resourceType, id) {
-      super(`${resourceType} with id ${id} was not found!`);
-      this.name = 'NotFoundError';
-    }
+  constructor(resourceType, id) {
+    super(`${resourceType} with id ${id} was not found!`); // Custom message
+    this.name = "NotFoundError";
   }
-  
-  export default NotFoundError;
-  
+}
+
+export default NotFoundError;
