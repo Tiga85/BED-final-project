3a742ec861337d557d7ca585d52053967ba64f4c
Tiga Shabani <tigana.saabani@gmail.com>
2024-09-13 13:28:38 +0200

    move sentry code to sentryInit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import "./utils/sentryInit.js";
 import * as Sentry from "@sentry/node";
 import express from "express";
 import "dotenv/config";
@@ -12,23 +13,7 @@ import bookingRoutes from "./routes/bookingRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
 import dataRoutes from "./routes/dataRoutes.js";
 import notFoundErrorHandler from "./middleware/notFoundErrorHandler.js";
-import errorHandler from "./errors/errorHandler.js";
-
-// Custom integration example
-class CustomIntegration {
-  setupOnce() {
-    console.log("Custom integration setup");
-  }
-}
-
-Sentry.init({
-  dsn: "https://0d82d19c2231f0a637d03a0044c55b9a@o4507921963679744.ingest.de.sentry.io/4507921985896528",
-  integrations: [new CustomIntegration()],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-});
+import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 app.use(express.json());
@@ -39,7 +24,7 @@ app.use("/api", userRoutes);
 app.use("/api", hostRoutes);
 app.use("/api", propertyRoutes);
 app.use("/api", amenityRoutes);
-app.use("/api", bookingRoutes);
+app.use("/api", bookingRoutes)
 app.use("/api", reviewRoutes);
 app.use("/api", dataRoutes);
 
