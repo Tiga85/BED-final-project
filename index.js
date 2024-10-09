4a1fc7995fdc3ff0e4b34d9853abfa2c2c771312
Tiga Shabani <tigana.saabani@gmail.com>
2024-10-10 00:23:59 +0200

    updated

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ import "dotenv/config";
 import { logMiddleware } from "./middleware/logMiddleware.js";
 import { router as loginRouter } from "./routes/loginRoutes.js";
 import { router as usersRouter } from "./routes/userRoutes.js";
-import hostRoutes from "./routes/hostRoutes.js";
+import { router as hostRouter } from "./routes/hostRoutes.js"; // Import hostRoutes from "./routes/hostRoutes.js";
 import propertyRoutes from "./routes/propertyRoutes.js";
 import amenityRoutes from "./routes/amenityRoutes.js";
-import bookingRoutes from "./routes/bookingRoutes.js";
-import reviewRoutes from "./routes/reviewRoutes.js";
+import { router as bookingRouter } from "./routes/bookingRoutes.js"; // Import bookingRoutes from "./routes/bookingRoutes.js";
+import { router as reviewRouter } from "./routes/reviewRoutes.js"; // Import reviewRoute from "./routes/reviewRoutes.js";
 import dataRoutes from "./routes/dataRoutes.js";
 import notFoundErrorHandler from "./middleware/notFoundErrorHandler.js";
 import errorHandler from "./middleware/errorHandler.js";
@@ -25,11 +25,11 @@ app.use(logMiddleware);
 app.use("/login", loginRouter);
 
 app.use("/users", usersRouter);
-app.use("/api", hostRoutes);
+app.use("/hosts", hostRouter);
 app.use("/api", propertyRoutes);
 app.use("/api", amenityRoutes);
-app.use("/api", bookingRoutes);
-app.use("/api", reviewRoutes);
+app.use("/bookings", bookingRouter);
+app.use("/reviews", reviewRouter);
 app.use("/api", dataRoutes);
 
 app.get("/", (req, res) => {
